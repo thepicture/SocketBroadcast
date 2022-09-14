4cd46d884bc043c8eec35839869b5bb93c4e2a44
thepicture <85333076+thepicture@users.noreply.github.com>
2022-09-14 17:19:28 +0500

    Add project files.

---
+++ SocketBroadcast.Client/Views/ChatWindow.xaml.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace SocketBroadcast.Client
+{
+    /// <summary>
+    /// Interaction logic for ChatWindow.xaml
+    /// </summary>
+    public partial class ChatWindow : Window
+    {
+        private Socket _socket;
+
+        public ChatWindow()
+        {
+            InitializeComponent();
+            IpV4Box.Text = IPAddress.Loopback.ToString();
+            PortBox.Text = 12345.ToString();
+        }
+
+
+        private void OnPortChanged(object sender, TextCompositionEventArgs e)
+        {
+            if (!e.Text.All(char.IsDigit))
+                e.Handled = true;
+        }
+
+        private void OnAddressChanged(object sender, TextCompositionEventArgs e)
+        {
+            if (!e.Text.All(c => char.IsDigit(c) || c == '.'))
+                e.Handled = true;
+        }
+
+        private void OnConnect(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                _socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                IPAddress address = IPAddress.Parse(IpV4Box.Text);
+                _socket.Connect(address, int.Parse(PortBox.Text));
+                Task.Run(() =>
+                {
+                    while (true)
+                    {
+                        try
+                        {
+                            byte[] buffer = new byte[1024];
+                            _socket.Receive(buffer);
+                            Dispatcher.Invoke(() => MessagesBox.Text += Encoding.UTF8.GetString(buffer) + '\n');
+                        }
+                        catch
+                        {
+                            break;
+                        }
+                    }
+                });
+                ConnectButton.IsEnabled = false;
+                DisconnectButton.IsEnabled = true;
+                ButtonSend.IsEnabled = true;
+                MessageBox.Show("Connected");
+            }
+            catch (Exception ex)
+            {
+                ConnectButton.IsEnabled = true;
+                DisconnectButton.IsEnabled = false;
+                ButtonSend.IsEnabled = false;
+                MessageBox.Show("Cannot connect. " + ex);
+            }
+        }
+
+        private void OnDisconnect(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (_socket.Connected)
+                    _socket.Close();
+                _socket.Dispose();
+                ConnectButton.IsEnabled = true;
+                DisconnectButton.IsEnabled = false;
+                ButtonSend.IsEnabled = false;
+                MessageBox.Show("Disconnected");
+            }
+            catch (Exception ex)
+            {
+                ConnectButton.IsEnabled = false;
+                DisconnectButton.IsEnabled = true;
+                ButtonSend.IsEnabled = true;
+                MessageBox.Show("Cannot discconnect. " + ex);
+            }
+        }
+
+        private void OnMessageSend(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(TextBoxMessage.Text))
+                return;
+            byte[] buffer = Encoding.UTF8.GetBytes($"{Environment.MachineName}: {TextBoxMessage.Text}");
+            try
+            {
+                _socket.Send(buffer);
+                TextBoxMessage.Text = string.Empty;
+            }
+            catch { }
+        }
+    }
+}
---
+++ SocketBroadcast.Server/Views/ServerWindow.xaml.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace SocketBroadcast.Server
+{
+    /// <summary>
+    /// Interaction logic for ServerWindow.xaml
+    /// </summary>
+    public partial class ServerWindow : Window
+    {
+        private Socket _socket;
+
+        public ServerWindow()
+        {
+            InitializeComponent();
+            IpV4Box.Text = IPAddress.Loopback.ToString();
+            PortBox.Text = 12345.ToString();
+        }
+
+        private void OnPortChanged(object sender, TextCompositionEventArgs e)
+        {
+            if (!e.Text.All(char.IsDigit))
+                e.Handled = true;
+        }
+
+        private void OnAddressChanged(object sender, TextCompositionEventArgs e)
+        {
+            if (!e.Text.All(c => char.IsDigit(c) || c == '.'))
+                e.Handled = true;
+        }
+
+        private void OnListen(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                _socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                EndPoint endPoint = new IPEndPoint(IPAddress.Parse(IpV4Box.Text), int.Parse(PortBox.Text));
+                _socket.Bind(endPoint);
+                _socket.Listen(1);
+                Task.Run(() =>
+                {
+                    try
+                    {
+                        Socket socket = _socket.Accept();
+                        while (true)
+                        {
+                            try
+                            {
+                                byte[] buffer = new byte[1024];
+                                socket.Receive(buffer);
+                                socket.Send(buffer);
+                            }
+                            catch
+                            {
+                                break;
+                            }
+                        }
+                    }
+                    catch { }
+                });
+                ListenButton.IsEnabled = false;
+                StopListeningButton.IsEnabled = true;
+                MessageBox.Show("Listening");
+            }
+            catch (Exception ex)
+            {
+                ListenButton.IsEnabled = true;
+                StopListeningButton.IsEnabled = false;
+                MessageBox.Show("Cannot listen. " + ex);
+            }
+        }
+
+        private void OnStopListening(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                _socket.Dispose();
+                ListenButton.IsEnabled = true;
+                StopListeningButton.IsEnabled = false;
+                MessageBox.Show("Stopped listening");
+            }
+            catch (Exception ex)
+            {
+                ListenButton.IsEnabled = false;
+                StopListeningButton.IsEnabled = true;
+
+                MessageBox.Show("Cannot stop listening. " + ex);
+            }
+        }
+    }
+}
